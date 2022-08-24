35568d14f9019a63314250aa6a2547779c7b4acd
Lev-i <lev_izjurov@rambler.ru>
2022-08-24 17:15:06 +0300

    realise sending data

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
 #include <W5500_driver.h>
 #include <sensor.h>
 
+//send data about ready sensors to host
+void eth_send();
+
 wiz_NetInfo gWIZNETINFO = 
 {
   .mac = {ETHADDR0, ETHADDR1, ETHADDR2, ETHADDR3, ETHADDR4, ETHADDR5},
@@ -35,35 +38,61 @@ void sysInit(){
   //Usart_print("Initialization complete");
 }
 
-uint8_t gDATABUF[DATA_BUF_SIZE];
-
-uint8_t error;
-char Message[128] = "Hello world, It's a Lev";
+uint8_t data[255];
+uint8_t data_length;
 int main( void )
 {
   sysInit();
-
-  uint8_t s_num = 12;
-  USART_SendData8(USART1,s_num);
-  s_get_data(0,&s_num);
-  USART_SendData8(USART1,s_num);
+  uint8_t s_num;
 
   while(1){
 
-
     // by timer
-    //s_num = check_sensors();
+    s_num = check_sensors();
 
-    if (s_num != 0){
-      //data = s_get_data(s_num);
-      //send();
-      //s_reset(s_num);
+    if (s_num != 255){
+      data_length = s_get_data(s_num, data);
+      eth_send();
+      reset_sensor(s_num);
     }
 
     // listenning
-
-
   }
   
   
+}
+
+void eth_send(){
+  
+  uint8_t error;
+  error = socket(SOCKET_0, Sn_MR_TCP, 80, 0);
+  
+  if(error != SOCKET_0){
+    Usart_print("Socket failed");
+    return;
+  }else{ 
+    Usart_print("Socket created, connecting...");
+  }
+      
+  uint8_t addr[4] = {S1_IPADDR0, S1_IPADDR1, S1_IPADDR2, S1_IPADDR3};
+  error = connect(HTTP_SOCKET,addr,DESTINATION_PORT);
+  
+  if(error != SOCK_OK){
+    Usart_print("Connection failed");
+    return;
+  }else{ 
+    Usart_print("Connection created");
+  }
+
+  error = send(0, data, data_length);
+  if(error != data_length){
+    Usart_print("Sending failed");
+    return;
+  }else{ 
+    Usart_print("data sended");
+  }
+  
+  disconnect(SOCKET_0);
+  close(SOCKET_0);
+  Usart_print("Close socket");
 }
